b0dc681cd12ea36d347b0feb6849d7779a644e35
Ardavan Hosseini <me@arho.ir>
2022-11-24 16:02:23 +0100

    Update sections

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,3 +1,13 @@
-$(".box-simple").on('click', function(event) {
-    
+$('.box-simple').click(function(){
+    $(this).toggleClass('box-simple-selected');
+    $(this).
+    console.log(this.className);
 });
+
+
+function updateTextBoxdevelopment() {
+    var informationBox = document.getElementById("development-info");
+    var informationTarget = document.getElementById("infobox")
+    informationTarget = informationBox.innerText;
+    informationTarget.innerHTML = informationBox.innerHTML;
+}
